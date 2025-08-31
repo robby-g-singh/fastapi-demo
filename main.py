2fe0fbe46e864c9207b57aceb48139a62f9e9784
Robby <singh.robbys13@gmail.com>
2025-08-31 17:14:55 -0500

    added POST endpoint to create a new user

--- main.py
+++ main.py
@@ -26,6 +26,23 @@ users = {
 }
 
 
+# Base Pydantic Models
+class User(BaseModel):
+    # validates the following data is passed of the correct types
+    name: str
+    website: str
+    age: int
+    role: str
+
+
+class UpdateUser(BaseModel):
+    # model for updating a user; optional choice for the user to update any or all fields
+    name: Optional[str] = None
+    website: Optional[str] = None
+    age: Optional[int] = None
+    role: Optional[str] = None
+
+
 # Endpoint creation:
 @app.get("/")  # create the landing page
 def root():
@@ -40,3 +57,21 @@ def get_user(user_id: int = Path(..., description="The ID you want to get", gt=0
     if user_id not in users:
         raise HTTPException(status_code=404, detail="User Not Found!")
     return users[user_id]
+
+
+# create a user
+# to store a user(s), need models to store them
+@app.post("/users/{user_id}", status_code=status.HTTP_201_CREATED)  # post to create a user;
+# 201 status = successful creation
+def create_user(user_id: int, user: User):  # function to create user; input parameters would need to be a user_id of
+    # type int and a user of type User as specified in the User Model above
+    if user_id in users:  # check for if the user exists in our makeshift database
+        raise HTTPException(status_code=400, detail="User already exists")  # if so raise a 400 status error
+    users[user_id] = user.dict()  # store user.dict() inside our users db with the key of user_id
+    return user
+
+# update a user
+
+# delete a user
+
+# search for a user
